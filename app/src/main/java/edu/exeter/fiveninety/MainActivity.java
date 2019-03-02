037ba543b0c45e9aefb4e09ef04154600f30e92a
vekt0r-github <victorluo@yahoo.com>
2019-03-01 20:34:18 -0500

    Update MainActivity.java

--- app/src/main/java/edu/exeter/fiveninety/MainActivity.java
+++ app/src/main/java/edu/exeter/fiveninety/MainActivity.java
@@ -19,10 +19,13 @@ public class MainActivity extends AppCompatActivity {
     int spectrumLength = 25;
     float[] spectrum = new float[spectrumLength];
     SoundPool sp;
-    int violin;
+    int violin, piano, violinStream;
     Context context;
     File dir;
 //    MediaPlayer[] mps = new MediaPlayer[spectrumLength];
+    float[] violinBounds = {0.5f, 2.0f}; // min, max frequencies/rates
+    float[] rowBounds = {0.5f, 2.0f};
+    float[] colBounds = {0.5f, 2.0f};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +38,12 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < spectrumLength; i++) {
             spectrum[i] = (float) Math.pow(1, Math.abs(i - (spectrumLength-1) / 2));
         }
-        violin = sp.load(this, R.raw.piano_a2, 0);
+        violin = sp.load(this, R.raw.violin_a2, 0);
+        piano = sp.load(this, R.raw.piano_a2, 0);
         sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                sounds(spectrum, 0.5f, 2.0f);
+                sounds(spectrum);
             }
         });
 //        Log.wtf("TAG", "" + soundID);
@@ -54,16 +58,55 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //        mps[1].start();
 //        mps[0].start();
+        violinStream = sp.play(violin, 1.0f, 1.0f, 100, 0, 1.0f);
         setContentView(R.layout.activity_main);
 //        sp.release();
     }
 
-    protected void sounds(float[] spectrum, float min, float max) {
+    protected void oldSounds(float[] spectrum, float min, float max) { // piano with changing volumes
         for (int i = 0; i < spectrumLength; i++) {
             float freq = min * (float) Math.pow(max / min, (float) i / (spectrumLength - 1));
-            sp.play(violin, spectrum[i], spectrum[i], 100, -1, freq);
+            sp.play(piano, f(spectrum[i]), f(spectrum[i]), 100, 0, freq);
         }
         // input amplitude array, min freq, max freq
         // make sounds loopable and short
+    }
+
+    protected float f(float x) { // function on spectrum inputs f:[0,1]->[0,1]
+        return (float) Math.pow(x, 2);
+    }
+
+    protected void sounds(float[][] diffArray) {
+        // violin: pitch higher when disturbed more
+        // row: first piano; plays a tone corresponding to the row with the greatest disturbance
+        // col: second piano; same but with columns
+        int width = diffArray[0].length;
+        int height = diffArray.length;
+        float avg = 0;
+        float[] rowAvg = new float[height];
+        float[] colAvg = new float[width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                avg += f(diffArray[i][j]) / (width * height);
+                rowAvg[i] += f(diffArray[i][j]) / width;
+                colAvg[j] += f(diffArray[i][j]) / height;
+            }
+        }
+        int maxRow = 0;
+        for (int i = 0; i < height; i++) {
+            if (rowAvg[i] > rowAvg[maxRow])
+                maxRow = i;
+        }
+        int maxCol = 0;
+        for (int j = 0; j < width; j++) {
+            if (colAvg[j] > colAvg[maxCol])
+                maxCol = j;
+        }
+        float freq = violinBounds[0] * (float) Math.pow(violinBounds[1] / violinBounds[0], avg);
+        float rowFreq = rowBounds[0] * (float) Math.pow(rowBounds[1] / rowBounds[0], maxRow / (height - 1));
+        float colFreq = colBounds[0] * (float) Math.pow(colBounds[1] / colBounds[0], maxCol / (width - 1));
+        sp.setRate(violinStream, freq);
+        sp.play(piano, rowAvg[maxRow], rowAvg[maxRow], 100, 0, rowFreq);
+        sp.play(piano, colAvg[maxCol], colAvg[maxCol], 100, 0, colFreq);
     }
 }
